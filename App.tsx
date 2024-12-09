1cc5bb8679bd313e931c3378bc4050573199ed08
noumanhafeez <noumanhafeez095@gmail.com>
2024-12-09 23:27:28 +0500

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SegmentedControl } from "./components/segmented-Control";
+import { useState } from "react";
+import BarAnimation from "./screens/BarAnimation";
+import ColorShift from "./screens/colorShift";
+import SortAnimation from "./screens/SortAnimation";
+import CircluarProgress from "./screens/CircluarProgress";
+import DropDownControl from "./screens/DropDownControl";
+import CustomBar from "./screens/CustomBar";
+import Magiclayout from "./screens/magiclayout";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="DropDown" component={DropDownControl} />
+        <Stack.Screen name="Magiclayout" component={Magiclayout} />
+        <Stack.Screen name="CustomBar" component={CustomBar} />
+        <Stack.Screen name="BarAnimation" component={BarAnimation} />
+        <Stack.Screen name="ColorShift" component={ColorShift} />
+        <Stack.Screen name="SortAnimation" component={SortAnimation} />
+        <Stack.Screen name="CircularProgress" component={CircluarProgress} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
